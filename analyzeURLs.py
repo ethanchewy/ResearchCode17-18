41c358ad1221e84b283017c22fc70b629bd0afb2
Ethan Chiu <17chiue@gmail.com>
2018-01-02 18:28:48 -0500

    Analyze each Reddit post/comment in depth using Selenium
    - Also reorganizes info for more info.
    Need to do: 
    - Finish reorganizing code
    - Clean up data frame
    - Export to csv cleanly. Test thoroughly

--- analyzeURLs.py
+++ analyzeURLs.py
@@ -12,6 +12,7 @@ import time
 
 #Comment out if you are collecting for the first time
 #Import CSV data so that you don't have to recollect all the data again
+'''
 colnames = ['links', 'subReddits', 'timestamp', 'info', 'snippets']
 data = pandas.read_csv('December2016November2017discordgg.csv', names=colnames)
 links = data.links.tolist()
@@ -20,6 +21,7 @@ timestamps = data.timestamp.tolist()
 #data.info is another function!
 infos = data["info"].tolist()
 snippets = data.snippets.tolist()
+'''
 #Note that lists' first element is the number indicating the position of the column in the csv file
 
 #Load Json
@@ -55,7 +57,99 @@ for what in timestamp:
 subReddits=[]
 for y in links:
 	subReddits.append(y.split('/')[4])
-	
+
+#Get data for Snippets
+snippets=[]
+for a in data:
+	for b in a['results']:
+		snippets.append(b['snippet'])
+
+#Processes data to get subreddit page and description
+#Gathers as much info as possible for subreddit info
+#Subreddits have different CSS so be careful
+#Notes: https://docs.google.com/document/d/1pegankZVLFGLRIqEpfdQwilZ_T-cQkm7dyeQdOltFgU/edit
+#THIS CODE ASSUMES THE BROWSER YOU ARE USING IS NOT LOGGED INTO REDDIT 
+originalPostContent = []
+titleOfRedditPost = []
+typeOfPost = []
+authorLink = []
+
+#list of lists of all the comments of the post: https://stackoverflow.com/questions/11487049/python-list-of-lists
+#Will not do right now due to time constraints
+completeThread = []
+
+#Does Google even scrape Reddit comments? Analyzed just in case.
+
+#Comments look like => https://www.reddit.com/r/science/comments/7n2zqt/study_of_550_college_students_who_had_used_or/dryyzir/
+#8th part of regex
+
+#For removing links remember to remove all data points
+#For loop to process each link
+
+#Uncomment this if you are analyzing new data and not importing.
+#for a, b, c in zip(links, snippets, timestamp):
+for a, b, c, d, e in zip(links, snippets, timestamp, subReddits, info):
+    
+    #Try else loop to see if the link is actually a comment or post 
+    #If not either, it's not reliable since it's probs related to a wiki page
+    #Thus, if it's something like a wiki page, that means the timestamp could be incorrect
+    try:
+        part = a.split('/')[5]
+        browser.get(a)
+        
+        '''
+        Try to see if it's a comment.
+        Process Comment in its entirety if so.
+        Find HTML element.
+
+        '''
+        
+        try:
+            partOfComment = a.split('/')[8]
+            typeOfPost = "Comment"
+            elem = browser.find_elements_by_class_name("commentarea")[0]
+            elem2 = elem.find_elements_by_class_name("sitetable")[0]
+            content = elem2.find_elements_by_class_name("thing")[0]      
+            theComment = content.find_elements_by_class_name("entry")[0]
+            contentOfComment = theComment.find_elements_by_class_name("usertext")[0].text
+            byLine = theComment.find_elements_by_class_name("tagline")[0]
+            authorHREF = byLine.find_elements_by_tag_name("a")[1].get_attribute("href")
+
+            originalPostContent.append(contentOfComment)
+            authorLink.append(authorHREF)
+            
+        #Process Post in its entirety
+        except IndexError:
+
+            typeOfPost = "Post"
+            
+            #Isloate elements and get texts
+            elem = browser.find_elements_by_class_name("entry")[0]
+            content = elem.find_elements_by_class_name("expando")[0].text      
+            frontmatter = elem.find_elements_by_class_name("top-matter")[0]
+            title = frontmatter.find_elements_by_class_name("title")[0].text
+            byLine = frontmatter.find_elements_by_class_name("tagline")[0]
+            authorHREF = byLine.find_elements_by_tag_name("a")[0].get_attribute("href")
+
+            #Append to correct indice
+            titleOfRedditPost.append(title)
+            authorLink.append(authorHREF)
+            originalPostContent.append(content)
+    except IndexError:
+        links.remove(a)
+        snippets.remove(b)
+        timestamp.remove(c)
+        #Remove bottom two lines of code if not importing already collected data
+        subReddits.remove(d)
+        info.remove(e)
+    time.sleep(3)
+    
+    
+print originalPostContent
+print titleOfRedditPost
+print typeOfPost 
+print authorLink
+
 # Path = where the "chromedriver" file is
 browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
 browser.get('https://www.reddit.com/reddits')
@@ -77,12 +171,7 @@ for channel in subReddits:
         info.append("")
     #Solution to stale element issue since element changes from the original q element
     inputBox = browser.find_element_by_name("q")
-	
-#Get data for Snippets
-snippets=[]
-for a in data:
-	for b in a['results']:
-		snippets.append(b['snippet'])
+
 	
 import json
 import pandas as pd
